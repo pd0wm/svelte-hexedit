67e8c0722e1784732383b569375c10c1bcab6241
Willem Melching <willem.melching@gmail.com>
2023-02-12 16:28:22 +0100

    remove unused packages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,5 @@
 <script>
-    // import '@fontsource/roboto/300.css';
-    // import '@fontsource/roboto/400.css';
-    // import '@fontsource/roboto/500.css';
-    // import '@fontsource/roboto/700.css';
-    // import '@material/typography/dist/mdc.typography.css';
     import 'svelte-material-ui/bare.css';
-
     import HexEditorBody from '../lib/HexEditorBody.svelte';
 
     let files;
