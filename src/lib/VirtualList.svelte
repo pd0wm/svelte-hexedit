74dec566c9d74ae2de525e3b75ab9b6d206822d8
Willem Melching <willem.melching@gmail.com>
2023-05-13 12:14:24 +0800

    use keyed each and increase number of extra rows

--- src/lib/VirtualList.svelte
+++ src/lib/VirtualList.svelte
@@ -5,7 +5,7 @@
     export let num_rows;
     export let row_height;
     export let height = 1024;
-    export let extra_rows = 5; // Extra rows to preload on either side of the visible rows
+    export let extra_rows = 100; // Extra rows to preload on either side of the visible rows
 
     let visible_rows = Math.ceil(height / row_height) + (2 * extra_rows);
     $: rows = range(0, Math.min(visible_rows + extra_rows, num_rows - 1));
@@ -26,7 +26,7 @@
     <div style="height:{height}px;width:100%;overflow:auto;" bind:this={box} on:scroll={parseScroll}>
         {#if num_rows > 0}
             <div style="display:flex;flex-direction:column; height:{num_rows * row_height}px;position:relative;">
-                {#each rows as i}
+                {#each rows as i (i)}
                     <slot name="item" style="height={row_height}px;position:absolute;transform:translateY({i * row_height}px);display:block;" index={i}></slot>
                 {/each}
             </div>
